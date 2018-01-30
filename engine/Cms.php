07675bf3d24e6dc45238d0df9fd2bbbfb69fc6e6
dzherihov <ddv672kn@gmail.com>
2018-01-30 11:02:25 +0300

    version 1

---
+++ engine/Cms.php
@@ -0,0 +1,26 @@
+<?php 
+
+namespace Engine;
+
+class Cms
+{
+	private $di;
+
+	/**
+	* Cms constructor.
+	* @param $di
+	*/
+	public function __construct($di)
+	{
+		$this->di = $di;
+	}
+
+
+	// Run CMS
+	public function run()
+	{
+
+	}
+}
+
+?>
---
+++ engine/bootstrap.php
@@ -0,0 +1,20 @@
+<?php 
+
+use Engine\Cms;
+use Engine\DI\DI;
+
+try{
+	//Dependency injection
+	$di = new DI();
+
+
+	$cms = new Cms($di);
+	$cms->run();
+
+}catch (\ErrorException $e)
+{
+	echo $e->getMessage();
+
+}
+
+?>
